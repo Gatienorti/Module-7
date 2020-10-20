761a14b0e9ab6c776f524904a8d2cb12fe04af3f
Gatien Orti-vinas <gatien.orti@gmail.com>
2020-10-20 18:50:14 -0400

    dada

---
+++ foundations-JS-7/foundations-JS-7/js/index.js
@@ -0,0 +1,14 @@
+// let car1 = "element";
+// let car2 = "FJ";
+// let car3 = "forflex";
+// let fe ='5'
+// let cars = ["element", "FJ", fe, "forflex"];
+// cars.pop()
+// console.log(cars)
+
+let myCar = {
+    "key": "tresasure",
+    "color": "blue"
+}
+
+console.log(myCar)
